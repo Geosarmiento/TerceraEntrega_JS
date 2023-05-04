cbe22a13a2ef671b639812d7ac2732156efbe4be
Geosarmiento <jhova29@gmail.com>
2023-05-04 10:25:22 -0500

    first commit

---
+++ stock.js
@@ -0,0 +1,34 @@
+
+let stockProductos = [
+
+    {id: 1, nombre: "Tshirt", 
+        tipo: "Camiseta", 
+        cantidad: 1, 
+        desc: "Camiseta Color Gris", 
+        precio: 25, talle: "L", 
+        img: './img/buzo.jpg'},
+   
+    {id: 7, 
+        nombre: "Tshirt", 
+        tipo: "Camiseta", 
+        cantidad: 1, 
+        desc: "Camiseta Color Negro", 
+        precio: 35, talle: "L", 
+        img: './img/remera.jpg'},
+    
+    {id: 13, 
+        nombre: "Tshirt",
+        tipo: "Camiseta",
+        cantidad: 1,
+        desc: "Camiseta Color Azul",
+        precio: 28, talle: "L", img: './img/camisa.jpg'},
+    
+    {id: 19,
+        nombre: "Tshirt",
+        tipo: "Camiseta",
+        cantidad: 1,
+        desc: "Camiseta Color Roja",
+        precio: 32, talle: "L",
+        img: './img/pantalon.jpg'},
+    
+]
