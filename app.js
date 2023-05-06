7ae8957f11ecef37f0c77d2cfe4a52451c45565e
Geosarmiento <jhova29@gmail.com>
2023-05-06 14:01:43 -0500

    ok

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 
 const contenedorProductos = document.getElementById('contenedor-productos')
-
-//TERCER PASO
-
 const contenedorCarrito = document.getElementById('carrito-contenedor')
-
-//SEXTO PASO
-
 const botonVaciar = document.getElementById('vaciar-carrito')
-
-
-//SEXTIMO PASO, MODIFICAR LOS CONTADORES
-
 const contadorCarrito = document.getElementById('contadorCarrito')
-
-//OCTAVO PASO
 const cantidad = document.getElementById('cantidad')
 const precioTotal = document.getElementById('precioTotal')
 const cantidadTotal = document.getElementById('cantidadTotal')
@@ -29,67 +17,61 @@ document.addEventListener('DOMContentLoaded', () => {
         actualizarCarrito()
     }
 })
-//SEXTO PASO
+
 botonVaciar.addEventListener('click', () => {
     carrito.length = 0
     actualizarCarrito()
 })
 
-//PRIMER PRIMER PASO, INYECTAR EL HTML
+// INYECTAR EL HTML
+
 stockProductos.forEach((producto) => {
     const div = document.createElement('div')
     div.classList.add('producto')
     div.innerHTML = `
-    <img src=${producto.img} alt= "">
-    <h3>${producto.nombre}</h3>
-    <p>${producto.desc}</p>
-    <p>Talle: ${producto.talle}</p>
-    <p class="precioProducto">Precio:$ ${producto.precio}</p>
-    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
-
-    `
+        <img src=${producto.img} alt= "">
+        <h3>${producto.nombre}</h3>
+        <p>${producto.desc}</p>
+        <p>Talle: ${producto.talle}</p>
+        <p class="precioProducto">Precio:$ ${producto.precio}</p>
+        <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>`
     contenedorProductos.appendChild(div)
 
-    //2 - SEGUNDO PASO, LUEGO DE QUE INSERTEMOS EL HTML EN EL DOM:
+   
     const boton = document.getElementById(`agregar${producto.id}`)
-    //Por cada elemento de mi array, creo un div, lo cuelgo, le pongo un id particular, una vez colgado
-    //le hago un get element by id (el de agregar) Obtengo el elemento y a dicho elemento le agregamos
-    //el add event listener
+  
 
     boton.addEventListener('click', () => {
-        //esta funcion ejecuta el agregar el carrito con la id del producto
+                                                                                 //esta funcion ejecuta el agregar el carrito con la id del producto
         agregarAlCarrito(producto.id)
         //
     })
 })
 
-// 1- PRIMER PASO
 
 //AGREGAR AL CARRITO
 const agregarAlCarrito = (prodId) => {
 
-    //PARA AUMENTAR LA CANTIDAD Y QUE NO SE REPITA
-    const existe = carrito.some (prod => prod.id === prodId) //comprobar si el elemento ya existe en el carro
+    const existe = carrito.some (prod => prod.id === prodId)                     //comprobar si el elemento ya existe en el carro
 
-    if (existe){ //SI YA ESTÁ EN EL CARRITO, ACTUALIZAMOS LA CANTIDAD
-        const prod = carrito.map (prod => { //creamos un nuevo arreglo e iteramos sobre cada curso y cuando
-            // map encuentre cual es el q igual al que está agregado, le suma la cantidad
+    if (existe){                                                                 //SI YA ESTÁ EN EL CARRITO, ACTUALIZAMOS LA CANTIDAD
+        const prod = carrito.map (prod => { 
             if (prod.id === prodId){
                 prod.cantidad++
             }
         })
-    } else { //EN CASO DE QUE NO ESTÉ, AGREGAMOS EL CURSO AL CARRITO
-        const item = stockProductos.find((prod) => prod.id === prodId)//Trabajamos con las ID
-        //Una vez obtenida la ID, lo que haremos es hacerle un push para agregarlo al carrito
+    } else {                                                                     //EN CASO DE QUE NO ESTÉ, AGREGAMOS EL CURSO AL CARRITO
+        const item = stockProductos.find((prod) => prod.id === prodId)          //Trabajamos con las ID
+                                                                                //Una vez obtenida la ID, lo que haremos es hacerle un push para agregarlo al carrito
         carrito.push(item)
     }
-    //Va a buscar el item, agregarlo al carrito y llama a la funcion actualizarCarrito, que recorre
-    //el carrito y se ve.
-    actualizarCarrito() //LLAMAMOS A LA FUNCION QUE CREAMOS EN EL TERCER PASO. CADA VEZ Q SE 
+                                                                                //Va a buscar el item, agregarlo al carrito y llama a la funcion actualizarCarrito, que recorre
+                                                                                //el carrito y se ve.
+    actualizarCarrito()                                                         //LLAMAMOS A LA FUNCION QUE CREAMOS EN EL TERCER PASO. CADA VEZ Q SE 
     //MODIFICA EL CARRITO
 }
-//agregarAlCarrito(1) //Le pasamos el ID por parametro. Tenemos que asigarle como evento esta funcion al boton
-//con el id de su producto correspondiente
+                                                                                //agregarAlCarrito(1) //Le pasamos el ID por parametro. Tenemos que asigarle como evento esta funcion al boton
+                                                                                //con el id de su producto correspondiente
 
 // 5 - QUINTO PASO
 const eliminarDelCarrito = (prodId) => {
